53922b77d4ced73e3e60cbf91f804adb1231079c
LuArtale <luartale@hotmail.com>
2020-11-04 17:29:08 -0300

    string env agregado

--- SkyPhotoTips/app/src/main/java/com/ea2soa/skyphototips/RegisterActivity.java
+++ SkyPhotoTips/app/src/main/java/com/ea2soa/skyphototips/RegisterActivity.java
@@ -64,7 +64,7 @@ public class RegisterActivity extends Activity {
                 Log.i("LOG_REGISTER","Starting Register User Request");
 
                 RequestRegisterUser requestRegisterUser = new RequestRegisterUser();
-                requestRegisterUser.setEnv("TEST"); //puede ser TEST o PROD
+                requestRegisterUser.setEnv(getString(R.string.enviroment)); //puede ser TEST o PROD
                 requestRegisterUser.setName(inputTextName.getText().toString());
                 requestRegisterUser.setLastname(inputTextLastname.getText().toString());
                 requestRegisterUser.setDni(Long.parseLong(inputTextDni.getText().toString()));
